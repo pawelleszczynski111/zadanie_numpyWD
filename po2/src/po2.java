50bf9a804836c6921e9d61522fd8c530a4a29393
pawelleszczynski111 <73653376+pawelleszczynski111@users.noreply.github.com>
2021-10-13 11:51:13 +0200

    Add files via upload

---
+++ po2/src/po2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class po2 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+//        zad1
+//        a)
+//        System.out.println("podaj n");
+//        int n=in.nextInt();
+//        int i, sum=0;
+//        for(i=0;i<=n;i++)
+//        {
+//            sum=sum+i;
+//        }
+//        System.out.println("wartosc: "+sum);
+
+//        b)\
+//        System.out.println("podaj n");
+//        int n=in.nextInt();
+//        int i, sum = 1;
+//        for (i = 1; i <= n; i++) {
+//            sum = sum * i;
+//        }
+//        System.out.println("wartosc: " + sum);
+
+//        c)
+//        System.out.println("podaj n");
+//        int n=in.nextInt();
+//        int i, sum = 0;
+//        for (i = 0; i <= n; i++) {
+//            sum = sum + abs(i);
+//        }
+//        System.out.println("wartosc: " + sum);
+
+    }
+}
